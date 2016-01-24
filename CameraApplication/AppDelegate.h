419fd20168ec5dfe0d092a0bf1ab4992b3e6dccd
Ash <ash@ashryanbeats.com>
2016-01-24 11:57:09 -0500

    Initial commit.

---
+++ CameraApplication/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CameraApplication
+//
+//  Created by Ash Ryan Arnwine on 1/24/16.
+//  Copyright © 2016 Ash Ryan Beats. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CameraApplication/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CameraApplication
+//
+//  Created by Ash Ryan Arnwine on 1/24/16.
+//  Copyright © 2016 Ash Ryan Beats. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
